a10e947650a98d26549298c3127b45b9864da0c1
Nidhi <nidhidhiman73@gmail.com>
2020-12-20 23:18:01 -0800

    First commit

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+  <h2 v-bind:innerHTML="val" v-bind:class="className"></h2>
+  <button v-on:click="testing('My Message')">Click Me </button>
+  <button v-on:click="changeColor">{{ btnText }}</button>
+  <ul>
+    <li v-for="emp in employees" v-bind:key="emp.id"> {{ emp.name }}</li>
+  </ul>
+
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  data(){
+    return {
+      val: 'This is the default value',
+      className: 'blueText',
+      btnText: 'Change to red',
+
+      employees: [
+      
+        {
+          id: 101,
+          name:'Nidhi'
+        },
+
+        {
+          id: 102,
+          name:'Shalu'
+        },
+
+        {
+          id: 103,
+          name:'Ankita'
+        },
+
+        {
+          id: 104,
+          name:'Viki'
+        },
+
+      ]
+    
+    }
+  },
+
+  
+  methods: {
+    testing: function(val){
+      alert(val);
+    },
+
+    changeColor: function(){
+
+
+     console.log(this.className);
+
+     this.btnText = this.btnText == 'Change to red' ? 'Change to blue' : 'Change to red' ;
+
+      if(this.className=="blueText")
+        this.className="redText"
+      else
+        this.className="blueText";
+    }
+  },
+
+
+}
+</script>
+
+<style scoped>
+
+.blueText{
+  color:blue;
+  background-color: lightcyan;
+}
+
+
+.redText{
+  color:red;
+  background-color: lightcyan;
+}
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
